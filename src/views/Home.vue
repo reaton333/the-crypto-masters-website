0419f7490660c2c22c0bede8feb4b2b95922be59
reaton333 <rosseaton92@gmail.com>
2021-03-26 08:12:06 -0400

    Finished podcast tune in section on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
           sm="7"
         >
           <v-card
-            class="pa-2"
+            class="pa-2 mt-8"
             elevation="0"
           >
             <v-card-title
@@ -53,29 +53,62 @@
           </v-card>
         </v-col>
       </v-row>
+      <v-row
+        class="pa-16"
+      ></v-row>
+      <v-row
+        class="pa-8"
+      ></v-row>
+      <v-row
+        align="center"
+        justify="space-around"
+      >
       <v-card
-        flat
+        class="pa-2 text-center primary"
+        align="center"
         tile
         width="100%"
-        class="text-center"
       >
-        <v-card-text>
-          <v-btn
-            v-for="podcast in podcasts"
-            :key="podcast.id"
-            class="mx-4 white--text space-around"
-            icon
-            :href="podcast.url"
+        <v-card-title
+          class="justify-center white--text text-h4"
+        >
+          {{ socialMessage }}
+        </v-card-title>
+        <v-divider
+          dark
+        ></v-divider>
+          <v-row
+          align="center"
+          justify="space-around"
           >
-            <v-icon 
-              size="72px"
-              color="secondary"
+            <v-col
+              class="pa-10"
+              v-for="podcast in podcasts"
+              :key="podcast.id"
             >
-              {{ podcast.icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
-      </v-card>
+              <v-btn
+                class="mx-4 white--text space-around"
+                icon
+                x-large
+                :href="podcast.url"
+              >
+                <v-icon 
+                  size="72px"
+                  color="secondary"
+                >
+                  {{ podcast.icon }}
+                </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-row>
+      <v-row
+        class="pa-16"
+      ></v-row>
+      <v-row
+        class="pa-8"
+      ></v-row>
   </v-container>
 </template>
 
@@ -91,11 +124,12 @@ export default {
       catchPhrase: 'Helping You Master an Understanding of Crypto Assets',
       subPhrase: 'We provide information about crypto assets to allow the public to make their own decisions',
       podcasts: [
+                { id: 0, icon: 'mdi-youtube', url: 'https://www.youtube.com/channel/UCyrKtJ25wtlemNHk5MG-9tQ' },
                 { id: 1, icon: 'mdi-spotify', url: 'https://open.spotify.com/show/38kIh4n39NvSbUTJew7rF4?si=p5LCiy0fTKuZipgXDNpFbg' },
                 { id: 2, icon: 'mdi-podcast', url: 'https://podcasts.apple.com/us/podcast/the-crypto-masters-podcast/id1507473032' },
                 { id: 3, icon: 'mdi-amazon', url: 'https://www.amazon.com/The-Crypto-Masters-Podcast/dp/B08K59B5WN' },
                 { id: 4, icon: 'mdi-google-podcast', url: 'https://podcasts.google.com/feed/aHR0cHM6Ly90aGVjcnlwdG9tYXN0ZXJzLnBvZGJlYW4uY29tL2ZlZWQueG1s' },
-                { id: 5, icon: 'mdi-youtube', url: 'https://www.youtube.com/channel/UCyrKtJ25wtlemNHk5MG-9tQ' },
+                
       ],
       socialMessage: 'TUNE IN TO OUR SHOW'
     }
