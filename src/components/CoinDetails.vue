dedee672bbb7a0e1c34095a68dbd963c1d652b70
Sean Boaden <sean.b0153@gmail.com>
2021-05-31 14:40:21 +0800

    Price formatter shows figures on low value coins

--- src/components/CoinDetails.vue
+++ src/components/CoinDetails.vue
@@ -644,14 +644,21 @@ export default {
             // return this.currentDate
         },
         formatPrice(value) {
-            // Create our number formatter.
-            var formatter = new Intl.NumberFormat('en-US', {
+            let formatOptions = {
                 style: 'currency',
                 currency: 'USD',
                 // These options are needed to round to whole numbers if that's what you want.
                 //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
                 //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
-            });
+            }
+
+            if (value < 0.01) {
+                formatOptions.minimumSignificantDigits = 2
+                formatOptions.maximumSignificantDigits = 2
+            }
+
+            // Create our number formatter
+            var formatter = new Intl.NumberFormat('en-US', formatOptions);
 
             return formatter.format(value)
         },
