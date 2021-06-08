100e0c5d6bf61957e2acd19de0d8a1c08a142f04
reaton333 <rosseaton92@gmail.com>
2021-06-07 21:08:18 -0400

    Fixed scroll behavior so you, in most cases, return to top of screen

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,18 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior: (to, from, savedPosition) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        selector: to.hash
+      };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
 })
 
 export default router
