9cce6df77ede2fd59310d593e35f2864e7ee8ed8
reaton333 <rosseaton92@gmail.com>
2021-11-20 06:56:27 -0500

    Removed unneeded css and divs

--- src/components/EpisodeList.vue
+++ src/components/EpisodeList.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    <!-- Start title and search bar -->
     <v-row
       align="center"
       justify="center"
@@ -19,6 +20,7 @@
       v-if="!errorString"
     >
       <v-row
+        
         align="center"
         justify="center"
       >
@@ -33,62 +35,61 @@
               append-outer-icon="mdi-magnify"
             ></v-text-field>
         </v-col>
-
       </v-row>
-      <div v-if="filteredEpisodes.length" class="episode-cards">
-        <div class="card" v-for="episode in filteredEpisodes" v-bind:key="episode">
-          <EpisodeCard :episodeData="episode" />
-        </div>
-      </div>
-      <div v-else-if="loading">
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-skeleton-loader
-              v-if="loading"
-              class="mx-auto mb-6"
-              max-width="344"
-              min-width="300"
-              type="card"
-          ></v-skeleton-loader>
-          <v-skeleton-loader
-              v-if="loading"
-              class="mx-auto"
-              max-width="344"
-              min-width="300"
-              type="card"
-          ></v-skeleton-loader>
-          <v-skeleton-loader
-              v-if="loading"
-              class="mx-auto"
-              max-width="344"
-              min-width="300"
-              type="card"
-          ></v-skeleton-loader>
-        </v-row>
-      </div>
-      <div v-else>
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <h2>No episodes match your search</h2>
-        </v-row>
-      </div>  
-    </div>
-    <div
-      v-else
-    >
+      <!-- END title and search bar -->
       <v-row
+        v-if="filteredEpisodes.length" class="episode-cards"
         align="center"
         justify="center"
       >
-        <h2
-          class="text-h2 font-weight-bold"
-        >Error Getting Episodes! Check back later.</h2>
+        <v-col class="card" v-for="episode in filteredEpisodes" v-bind:key="episode">
+          <EpisodeCard :episodeData="episode" />
+        </v-col>
       </v-row>
-    </div>  
+      <v-row
+        v-else-if="loading"
+        align="center"
+        justify="center"
+      >
+        <v-skeleton-loader
+            v-if="loading"
+            class="mx-auto mb-6"
+            max-width="344"
+            min-width="300"
+            type="card"
+        ></v-skeleton-loader>
+        <v-skeleton-loader
+            v-if="loading"
+            class="mx-auto"
+            max-width="344"
+            min-width="300"
+            type="card"
+        ></v-skeleton-loader>
+        <v-skeleton-loader
+            v-if="loading"
+            class="mx-auto"
+            max-width="344"
+            min-width="300"
+            type="card"
+        ></v-skeleton-loader>
+      </v-row>
+      <v-row
+        v-else
+        align="center"
+        justify="center"
+      >
+        <h2>No episodes match your search</h2>
+      </v-row> 
+    </div>
+    <v-row
+      v-else
+      align="center"
+      justify="center"
+    >
+      <h2
+        class="text-h2 font-weight-bold"
+      >Error Getting Episodes! Check back later.</h2>
+    </v-row>
   </v-container>
 </template>
 
@@ -167,56 +168,4 @@ export default {
 </script>
 
 <style>
-
-.wrap {
-    width: 50%;
-    margin: 0 auto;
-    /* padding-bottom: 5%; */
-}
-
-
-.card {
-  width: 25%;
-  margin-bottom: 50px;
-  transition: 0.2s;
-}
-
-.cards-container {
-  width: 90%;
-  margin: 50px auto;
-}
-
-.episode-cards {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  flex-flow: wrap;
-}
-
-@media screen and (max-width: 2000px) {
-  .card {
-    width: 30%;
-  }
-}
-
-@media screen and (max-width: 1000px) {
-  .card {
-    width: 40%;
-  }
-}
-
-@media screen and (max-width: 620px) {
-  .episode-cards {
-    width: 100%;
-  }
-
-  .heading {
-    padding: 20px;
-    font-size: 20px;
-  }
-
-  .card {
-    width: 80%;
-  }
-}
 </style>
